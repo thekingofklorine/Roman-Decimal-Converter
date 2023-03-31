787aa116c49f117e9de2bf96952f46f49d832c17
“Evan” <evan.sportsman@gmail.com>
2023-03-30 22:59:02 -0600

    optimization and formatting

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Roman to Decimal and Back:
   A Python script that converts between Roman and Decimal numbers, with a value of less then 3,999. Written in Python3  
-Time to Code: 1.5 hrs  
+Time to Code: 2.5 hrs  
 ### Instructions:  
 <ol>
 <li>DOWNLOAD file to a system that has Python3 installed</li>
@@ -12,3 +12,7 @@ Time to Code: 1.5 hrs
   * If a decimal number is entered that is larger than 3,999 is entered it prompts for a new number
   * If a not valid character is included or a mix of decimal and roman is entered it prompts for a new number
   * If a space is included in the number it prompts for a new number
+  * If a improper Roman numeral is entered it prompts for a new number
+
+### Future Steps:
+  I would take a look at condensing the function decToRoman() since it has a lot of repeated code.
--- roman.py
+++ roman.py
@@ -1,134 +1,144 @@
+letterToNumberDict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
 
 def convertToNumber(x):
-    if x == "I":
-        return 1
-    elif x == "V":
-        return 5
-    elif x == "X":
-        return 10
-    elif x == "L":
-        return 50
-    elif x == "C":
-        return 100
-    elif x == "D":
-        return 500
-    elif x == "M":
-        return 1000   
+    if x not in letterToNumberDict.keys():
+        return float("inf")
     else:
-        return float('inf')
+        return letterToNumberDict.get(x)
 
 
-print("Welcome the the Roman Numeral to Decimal conversion Calculator, it goes both ways! Type either a roman numeral or a decimal number smaller then 3999 and hit enter. type quit and hit enter when finished")
-
-while(True):
-    print("Please type in your number")
-    x = input()
-    if(x == "quit"):
-        print("quitting...")
-        break
-    if(x.isnumeric()):
+def checkInputType(x):
+    if x == "quit":
+        numberType = "q"
+    elif x.isnumeric():
         numberType = "d"
-    elif(x.isalpha()):
+    elif x.isalpha():
         x = x.upper()
         numberType = "r"
-    else:
-        print("make sure you either a roman or decimal number and nothing else")    #bad inputs
+    else:  # bad input
+        numberType = "z"
+    return numberType
+
+
+def romanToDec(number):
+    total = 0
+    numberList = [convertToNumber(x) for x in number]
+    if float("inf") in numberList:
+        print("the number is too big")
+        return float("inf")
+    total += numberList[-1]  # add in the first number and set the prev
+    prev = numberList[-1]
+    for i in range(len(numberList) - 2, -1, -1):
+        if prev > numberList[i]:  # we have a larger number before
+            total -= numberList[i]  # subtract out this number (IV = 1,5 = 5 -  = 4)
+            prev = numberList[i]  # change our prev num
+        else:
+            total += numberList[i]  # add it on
+            prev = numberList[i]  # change our prev num
+    if total > 3999:
+        print("the number must be less then 4000")
+        return float("inf")
+    return total
+
+
+def decToRoman(number):
+    total = []
+    if number >= 1000:
+        y = number // 1000
+        number = number % 1000
+        for _ in range(y):
+            total.append("M")
+    if number >= 900:
+        y = number // 900
+        number = number % 900
+        for _ in range(y):
+            total.append("CM")
+    if number >= 500:
+        y = number // 500
+        number = number % 500
+        for _ in range(y):
+            total.append("D")
+    if number >= 400:
+        y = number // 400
+        number = number % 400
+        for _ in range(y):
+            total.append("CD")
+    if number >= 100:
+        y = number // 100
+        number = number % 100
+        for _ in range(y):
+            total.append("C")
+    if number >= 90:
+        y = number // 90
+        number = number % 90
+        for _ in range(y):
+            total.append("XC")
+    if number >= 50:
+        y = number // 50
+        number = number % 50
+        for _ in range(y):
+            total.append("L")
+    if number >= 40:
+        y = number // 40
+        number = number % 40
+        for _ in range(y):
+            total.append("XL")
+    if number >= 10:
+        y = number // 10
+        number = number % 10
+        for _ in range(y):
+            total.append("X")
+    if number >= 9:
+        y = number // 9
+        number = number % 9
+        for _ in range(y):
+            total.append("IX")
+    if number >= 5:
+        y = number // 5
+        number = number % 5
+        for _ in range(y):
+            total.append("V")
+    if number >= 4:
+        y = number // 4
+        number = number % 4
+        for _ in range(y):
+            total.append("IV")
+    if number >= 1:
+        y = number // 1
+        for _ in range(y):
+            total.append("I")
+    return total
+
+
+print(
+    "Welcome the the Roman Numeral to Decimal conversion Calculator, it goes both ways! Type either a roman numeral or a decimal number smaller then 3999 and hit enter. type quit and hit enter when finished"
+)
+while True:
+    print("Please type in your number")
+    x = input()
+    numberType = checkInputType(x)
+    if numberType == "q":
+        print("quitting...")
+        break
+    if numberType == "z":
+        print(
+            "make sure you either a roman or decimal number and nothing else"
+        )  # bad input detected
         continue
-        
-    number = x
-    if(numberType == "r"):  #do roman to decimal
-        total = 0
-        letterList = [i for i in number]
-        numberList = [convertToNumber(x) for x in letterList]
-        if(float('inf') in numberList):
-            print("the number is too big")
-            continue    
-        total += numberList[-1]     #add in the first number and set the prev
-        prev = numberList[-1]
-        for i in range(len(numberList) - 2, -1, -1):
-            if(prev > numberList[i]):   #we have a larger number before 
-                total -= numberList[i]  #subtract out this number (IV = 1,5 = 5 -  = 4)
-                prev = numberList[i]    #change our prev num
+    if numberType == "r":  # do roman to decimal
+        total = romanToDec(x)
+        if total != float("inf"):
+            test = decToRoman(total)
+            if "".join(test) == x:  # check if x is a legal roman numeral
+                print("The decimal equivalent is: " + str(total))
             else:
-                total += numberList[i]  #add it on
-                prev = numberList[i]    #change our prev num
-        if(total > 3900):
-            print("the number must be less then 3999")
+                print("not a valid roman numeral")
+    else:  # decimal to roman
+        x = int(x)
+        if x > 3999:
+            print("the number must be less then 4000")
             continue
-        print("The decimal equivalent is: " + str(total))
-    elif(numberType == "d"):   #decimal to roman
-        number = int(number)
-        if(number > 3900):
-            print("the number must be less then 3999")
-            continue
-        total = []
-        if(number >= 1000):
-            y = number // 1000
-            number = number % 1000
-            for i in range(y):
-                total.append("M")
-        if(number >= 100):
-            y = number // 900
-            number = number % 900
-            for i in range(y):
-                total.append("CM")
-        if(number >= 100):
-            y = number // 500
-            number = number % 500
-            for i in range(y):
-                total.append("D")
-        if(number >= 100):
-            y = number // 400
-            number = number % 400
-            for i in range(y):
-                total.append("CD")
-        if(number >= 100):
-            y = number // 100
-            number = number % 100
-            for i in range(y):
-                total.append("C")
-        if(number >= 10):
-            y = number // 90
-            number = number % 90
-            for i in range(y):
-                total.append("XC")
-        if(number >= 10):
-            y = number // 50
-            number = number % 50
-            for i in range(y):
-                total.append("L")
-        if(number >= 10):
-            y = number // 40
-            number = number % 40
-            for i in range(y):
-                total.append("XL")
-        if(number >= 10):
-            y = number // 10
-            number = number % 10
-            for i in range(y):
-                total.append("X")
-        if(number >= 1):
-            y = number // 9
-            number = number % 9
-            for i in range(y):
-                total.append("IX")
-        if(number >= 1):
-            y = number // 5
-            number = number % 5
-            for i in range(y):
-                total.append("V")
-        if(number >= 1):
-            y = number // 4
-            number = number % 4
-            for i in range(y):
-                total.append("IV")
-        if(number >= 1):
-            y = number // 1
-            for i in range(y):
-                total.append("I")
-        myString = ''.join(total)        
+        total = decToRoman(x)
+        myString = "".join(total)
         print("The roman equivalent is: " + myString)
-    else:   
-        print("make sure you place r or d after your number")   #if the letter is not r or d    
-
-
